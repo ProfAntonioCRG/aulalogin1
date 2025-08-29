865f9118b36f562e5d0b9beea6f3027a30bacc83
ProfAntonioCRG <163427510+ProfAntonioCRG@users.noreply.github.com>
2025-08-29 10:52:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function fazerLogin() {
+  const usuario = document.getElementById("usuario").value;
+  const senha = document.getElementById("senha").value;
+
+  if (usuario === "antonio" && senha === “3011") {
+    alert("✅ Login realizado com sucesso!");
+  } else {
+    alert("❌ Usuário ou senha incorretos!");
+  }
+}
 
